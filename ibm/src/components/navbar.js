bbb7b289d432e6fc4632a21133507b0ba053c0ee
khaylah <khaylahrscholar@gmail.com>
2023-01-30 19:04:17 -0600

    navbar linking

--- ibm/src/components/navbar.js
+++ ibm/src/components/navbar.js
@@ -1,6 +1,9 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+
+import {LinkContainer} from 'react-router-bootstrap';
+import { Outlet} from "react-router-dom";
 import '../css/navbar.css';
 
 import logo from '../img/mp.png'
@@ -10,15 +13,27 @@ function nav() {
     <>
       <Navbar id="nav" bg="light" variant="light">
         <Container>
+        <LinkContainer to="/Home">
           <Navbar.Brand href="#home"><img src={logo} style={{height:"80px"}} alt='logo' /></Navbar.Brand>
+        </LinkContainer>
+
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">About</Nav.Link>
-            <Nav.Link href="#pricing">Solution</Nav.Link>
-            
+          <LinkContainer to="/Home">
+            <Nav.Link>Home</Nav.Link>
+          </LinkContainer>
+
+          <LinkContainer to="/About">
+            <Nav.Link>About</Nav.Link>
+          </LinkContainer>
+
+          <LinkContainer to="/Solution">
+            <Nav.Link>Solution</Nav.Link>
+          </LinkContainer>
+          
           </Nav>
         </Container>
       </Navbar>
+      <Outlet />
     </>
   );
 }
