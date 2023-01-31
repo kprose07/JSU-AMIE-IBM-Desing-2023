f70fac97440f0109e74ce27ab35109c79d159139
khaylah <khaylahrscholar@gmail.com>
2023-01-30 18:29:45 -0600

    Update Navbar

--- ibm/src/components/navbar.js
+++ ibm/src/components/navbar.js
@@ -3,12 +3,14 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import '../css/navbar.css';
 
+import logo from '../img/mp.png'
+
 function nav() {
   return (
     <>
       <Navbar id="nav" bg="light" variant="light">
         <Container>
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Navbar.Brand href="#home"><img src={logo} style={{height:"80px"}} alt='logo' /></Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
             <Nav.Link href="#features">About</Nav.Link>
